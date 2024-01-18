542a3a9811454bea8a75ad4ae7b5ba7b95d5e1f3
cagataysen <orhancagataysen@outlook.com>
2024-01-18 22:58:45 +0300

    my-first-react-component-added

--- 02-react-practise/index.js
+++ 02-react-practise/index.js
@@ -1,4 +1,14 @@
+/* Surprise! You probably thought you could just forget
+the line of code you just learned! Nope, not on my watch!
+
+Try to write that 1-liner of React code again! This time,
+see if you can figure out how to render an <ul> with 2+ <li>s inside*/
 ReactDOM.render(
-  <h1>Hello React my old friend...</h1>,
+  <ul>
+    <li>Aleksandra</li>
+    <li>Jest</li>
+    <li>Kochanie</li>
+    <li>Moja</li>
+  </ul>,
   document.getElementById("root")
 );
