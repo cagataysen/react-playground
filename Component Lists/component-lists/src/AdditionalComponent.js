327efe5003b72ae966581b3396e1f8aa3cfea356
cagataysen <orhancagataysen@outlook.com>
2024-01-28 18:59:25 +0300

    button and image components practised

---
+++ Component Lists/component-lists/src/AdditionalComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AdditionalComponent = () => {
+  return (
+    <div className="image-component">
+      <br />
+      <h3>It's an image in react as an external page</h3>
+      <img src="https://i.imgur.com/fXWpjF8.jpg" alt="Image-example" />
+    </div>
+  );
+};
+
+export default AdditionalComponent;
