d60e38d9d688f8e742f76f70cdc628dbf88f3610
Cagatay Sen <orhancagataysen@outlook.com>
2024-08-21 14:47:56 +0200

    card component created

---
+++ BroCode-CardComponents/card-components/README.md
@@ -0,0 +1,14 @@
+# React + Vite
+
+How I created this react project: 
+ 
+
+- npm create vite@latest
+
+- then i gave a name to the project which is card-components
+
+- cd card-components
+
+- npm install
+
+- npm run dev
---
+++ BroCode-CardComponents/card-components/src/Card.jsx
@@ -0,0 +1,17 @@
+function Card() {
+
+    return(
+        <div className="card">
+
+            <img className="card-image" src="https://media.licdn.com/dms/image/v2/D4D03AQE92Ww4PwDqoQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1696114767373?e=1729728000&v=beta&t=1kAAXIBSZRmmYPtcZbYdIs8IeSWqkbDxkyCNTK8ZqeU" alt="profile picture" width="150" height="150"></img>
+            <h2 className="card-title">Yo its Cagatay</h2>
+            <p className="card-text">I am studying react with this project</p>
+
+
+        </div>
+
+    );
+
+}
+
+export default Card
