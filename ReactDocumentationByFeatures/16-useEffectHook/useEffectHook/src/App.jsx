155b6340281b0ca51258b77ef64e81251dc58225
Cagatay Sen <orhancagataysen@outlook.com>
2024-08-29 12:45:24 +0200

    useEffect example added

---
+++ ReactDocumentationByFeatures/16-useEffectHook/useEffectHook/src/App.jsx
@@ -0,0 +1,13 @@
+import MyComponent from "./MyComponent.jsx";
+import MyComponent2 from "./MyComponent2.jsx";
+
+function App() {
+  return (
+    <>
+      <MyComponent />
+      <MyComponent2 />
+    </>
+  );
+}
+
+export default App;
